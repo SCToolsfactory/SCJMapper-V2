0c1833451b39799358dd3bdb8ff3e88839c4c01b
standardtoaster <andrew.preece@gmail.com>
2014-06-22 11:30:09 -0700

    [joystickcls] cleaned up includes

--- Joystick/JoystickCls.cs
+++ Joystick/JoystickCls.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SharpDX;
+using SharpDX.DirectInput;
+using System;
 using System.Text;
 using System.Windows.Forms;
-using SharpDX;
-using SharpDX.DirectInput;
 
 namespace SCJMapper_V2
 {
